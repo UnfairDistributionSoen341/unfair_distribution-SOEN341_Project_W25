eb1aec073aab09dd32281763caf62d0c9a7cbb2e
Ali-B-Issa <157230188+Ali-B-Issa@users.noreply.github.com>
2025-02-15 20:05:47 -0500

    Update ServerManagementSystems.js

--- Sprint2/DirectMessaging/ServerManagementSystems.js
+++ Sprint2/DirectMessaging/ServerManagementSystems.js
@@ -7,7 +7,7 @@ class User {
 class Member extends User {
   constructor(username) {
         super(username);
-        this.role = "member"; // Override role to "owner"
+        this.role = "member"; // Override role to "member"
     }
   viewMessages(channel) {
     if (!channel) return [];
@@ -23,6 +23,41 @@ class Member extends User {
 
 // Admin class (inherits from User)
 class Admin extends User {
+  constructor(username) {
+        super(username);
+        this.role = "admin";// Override role to "owner"
+        this.servers = [];
+    }
+  // Server Management
+  createServer(serverName) {
+    const server = new Server(serverName, this);
+    this.servers.push(server);
+    return server;
+  }
+
+  deleteServer(server) {
+    const index = this.servers.findIndex(s => s.name === server.name);
+    if (index !== -1) this.servers.splice(index, 1);
+  }
+
+  // Channel Management
+  createChannel(server, channelName) {
+    server.addChannel(channelName);
+  }
+
+  deleteChannel(server, channelName) {
+    server.removeChannel(channelName);
+  }
+
+  // Team Management
+  createTeam(server, teamName) {
+    server.addTeam(teamName);
+  }
+
+  // Assign Users to Channels
+  assignUserToChannel(user, channel) {
+    channel.addUser(user);
+  }
 
 }
 
