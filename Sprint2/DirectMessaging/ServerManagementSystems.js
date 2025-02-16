9bd1a5c6a6cef51890e05333cd95696ddb03d8fe
Aaida Hossain <aaida.hossain1@gmail.com>
2025-02-15 19:42:52 -0500

    Update ServerManagementSystems.js

--- Sprint2/DirectMessaging/ServerManagementSystems.js
+++ Sprint2/DirectMessaging/ServerManagementSystems.js
@@ -1,6 +1,25 @@
 // Base User class
 class User {
+    constructor(username, role) {
+        this.username = username;
+        this.role = role;
+        this.assignedChannels = [];
+    }
+  
+    // Assign user to a channel
+    assignToChannel(channel) {
+        if (!this.assignedChannels.includes(channel)) {
+            this.assignedChannels.push(channel);
+            console.log(`${this.username} has been assigned to ${channel}.`);
+        } else {
+            console.log(`${this.username} is already assigned to ${channel}.`);
+        }
+    }
 
+    // Logout functionality
+    logout() {
+        console.log(`${this.username} has logged out.`);
+    }
 }
 
 // Member class (inherits from User)
@@ -15,10 +34,17 @@ class Admin extends User {
 
 // Owner class (inherits from Admin)
 class Owner extends Admin {
-
+    constructor(username) {
+        super(username);
+        this.role = "owner"; // Override role to "owner"
+    }
 }
 
 // Server class
 class Server {
+      constructor(username, role) {
+        this.username = username;
+        this.role = role;
+        this.assignedChannels = [];
 }
 
