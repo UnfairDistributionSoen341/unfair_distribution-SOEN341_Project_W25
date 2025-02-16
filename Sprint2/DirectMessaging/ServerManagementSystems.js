581b08e384029ac72e7aa92aa16769460fba3de4
Aaida Hossain <aaida.hossain1@gmail.com>
2025-02-15 19:27:37 -0500

    Update ServerManagementSystems.js

--- Sprint2/DirectMessaging/ServerManagementSystems.js
+++ Sprint2/DirectMessaging/ServerManagementSystems.js
@@ -1,1 +1,24 @@
+// Base User class
+class User {
+
+}
+
+// Member class (inherits from User)
+class Member extends User {
+
+}
+
+// Admin class (inherits from User)
+class Admin extends User {
+
+}
+
+// Owner class (inherits from Admin)
+class Owner extends Admin {
+
+}
+
+// Server class
+class Server {
+}
 
