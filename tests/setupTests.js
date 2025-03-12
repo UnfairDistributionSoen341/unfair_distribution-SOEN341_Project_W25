d29a715a9b873ee0793a8677dd965a2c6f8e60f2
Aaida Hossain <aaida.hossain1@gmail.com>
2025-03-12 10:58:49 -0400

    Update setupTests.js

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -4,10 +4,17 @@
 // Mock Firebase globally
 global.initializeApp = jest.fn(() => ({}));
 global.getAuth = jest.fn(() => ({}));
-global.createUserWithEmailAndPassword = jest.fn();
+global.createUserWithEmailAndPassword = jest.fn().mockResolvedValue({
+  user: { uid: 'test-uid-123' }
+});
 global.getDatabase = jest.fn(() => ({}));
-global.ref = jest.fn();
-global.set = jest.fn(() => Promise.resolve());
+global.ref = jest.fn(() => ({}));
+global.set = jest.fn().mockResolvedValue(undefined);
+global.child = jest.fn(() => ({}));
+global.get = jest.fn().mockResolvedValue({ 
+  exists: () => true, 
+  val: () => ({}) 
+});
 
 // Silence console errors in tests (they're expected in some error test cases)
 beforeEach(() => {
