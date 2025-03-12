0d5389077be859b2a164393243d91631ec0f2160
Aaida Hossain <aaida.hossain1@gmail.com>
2025-03-12 10:40:53 -0400

    Update database.js

--- tests/_mocks_/firebase/database.js
+++ tests/_mocks_/firebase/database.js
@@ -1,6 +1,6 @@
 // Mocked firebase/database module
 module.exports = {
   getDatabase: jest.fn(() => ({})),
-  ref: jest.fn(() => ({})),
+  ref: jest.fn(() => "mocked-ref"),
   set: jest.fn(() => Promise.resolve())
 };
