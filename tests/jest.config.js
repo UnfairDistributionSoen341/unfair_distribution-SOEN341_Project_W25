64fc1eb5aeb15fe2f3b37217f6098e7d462e91fc
Aaida Hossain <aaida.hossain1@gmail.com>
2025-03-12 06:02:29 -0400

    Create jest.config.js

---
+++ tests/jest.config.js
@@ -0,0 +1,49 @@
+// tests/jest.config.js
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js'],
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    // Map imports from SignUpPage directory
+    '^../SignUpPage/(.*)
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.js'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: 'coverage'
+};
+: '<rootDir>/Sprint1/SignUpPage/$1',
+    // Mock Firebase modules
+    '^firebase/app
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.js'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: 'coverage'
+};
+: '<rootDir>/__mocks__/firebase/app.js',
+    '^firebase/auth
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.js'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: 'coverage'
+};
+: '<rootDir>/__mocks__/firebase/auth.js',
+    '^firebase/database
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.js'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: 'coverage'
+};
+: '<rootDir>/__mocks__/firebase/database.js'
+  },
+  roots: ['<rootDir>'],
+  testMatch: ['**/*.test.js'],
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: 'coverage'
+};
